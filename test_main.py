656ce4162b2732e4b7f619f7a679d4f576e2d3b6
Nabil Shafi <nabil.shafi1@gmail.com>
2021-03-30 00:53:38 +0100

    change the class name

--- test_main.py
+++ test_main.py
@@ -1,12 +1,12 @@
 import unittest
-from main import *
+from web_analyzer import *
 
 class TestMain(unittest.TestCase):
 
 
     def test_url_status(self):
-        self.assertTrue(url_status("https://pypi.org/project/build/"))
-        self.assertFalse(url_status("youtube.com"))
+        self.assertTrue(url_status("https://www.youtube.com"))
+        self.assertTrue(url_status("http://youtube.com"))
         self.assertFalse(url_status("youtube"))
         self.assertFalse(url_status("https://www.youtubeeeee.com"))
         self.assertFalse(url_status("#"))
@@ -16,6 +16,7 @@ class TestMain(unittest.TestCase):
         page = urlopen(url)
         html = page.read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
-        gather_links(url, soup)
+        internal_links, external_links, all_links = gather_links(url, soup)
         self.assertEqual(len(internal_links),3)
+        self.assertEqual(len(external_links),0)
 
