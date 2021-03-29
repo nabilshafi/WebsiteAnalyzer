62cd703ea590d44e98f9dbd20e74172674563ba8
nabilshafi <nabil.shafi1@gmail.com>
2021-03-29 18:00:46 +0100

    test

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# This is a sample Python script.
-import re
+import re, sys
 from urllib.error import HTTPError, URLError
+from urllib.parse import urlparse
 from urllib.request import urlopen
-
 import validators
 from bs4 import BeautifulSoup
 
@@ -10,13 +9,9 @@ internal_links = set()
 external_links = set()
 all_links = set()
 
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# Checking the url status
 def url_status(url):
-
-    if validators.url(url):
+    if validators.url(url): # Validating the url format
         try:
             if urlopen(url).getcode() == 200:
                 return True
@@ -26,46 +21,68 @@ def url_status(url):
             return False
     return False
 
+# Gathering all links inside webpage and dividing accordingly
 def gather_links(url, soup):
-
-    domain_name = url.split(".")[1] + "." + url.split(".")[2]
+    host = urlparse(url).hostname
+    if host.startswith('www.'):
+        host = host[4:]
+    if host.endswith("/"):
+        host = host[:-1]
     pattern = re.compile("^(/)")  # For relative links
     for link in soup.find_all("a", href=True):
-        if re.findall(pattern, link['href']):  # Finding relative links
+        if re.findall(pattern, link['href']):  #Finding relative links
             new_url = url + link['href']
             internal_links.add(new_url)
-        elif domain_name not in link['href'] and validators.url(link['href']):
+        elif host not in link['href'] and validators.url(link['href']): #Finding the external links
             external_links.add(link['href'])
         else:
             internal_links.add(link['href'])
         all_links.add(link['href'])
 
-def reachable_links():
 
+
+
+# Verifying that links are reachabl
+def reachable_links():
     reach = 0
     for link in external_links:
         if url_status(link):
             reach += 1
     return reach
 
+#Checking the URL version
+def check_version(soup):
+    if "<!DOCTYPE" in str(soup):
+        partitioned_string = str(soup).split('>')[0]
+        if "HTML 4" in partitioned_string:
+            print("Website using HTML 4")
+        elif "XHTML" in partitioned_string:
+            print("Website using XTML")
+        else:
+            print("Website using HTML 5")
+        return 1
+    return 0
+
 if __name__ == '__main__':
 
-    url = "https://www.oreilly.com/"
+    url = sys.argv[1]
     if url_status(url):
         page = urlopen(url)
         html = page.read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
         gather_links(url, soup)
         reach = reachable_links()
+        forms = soup.find('form')
 
+        # Print statements
+        if not check_version(soup):
+            print("Version not found")
         print("Tittle: " + soup.title.string)
         print("No. of distinct links: " + str(len(all_links)))
         print("No. of external links: " + str(len(external_links)))
         print("No. of external reachable links: " + str(reach))
-
-        print(external_links)
-
+        if 'login' in str(forms).lower():
+            print("Login form exists")
     else:
         print("Invalid URL")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- test_main.py
+++ test_main.py
@@ -3,11 +3,19 @@ from main import *
 
 class TestMain(unittest.TestCase):
 
+
     def test_url_status(self):
-        self.assertTrue(url_status("https://www.youtube.com"))
+        self.assertTrue(url_status("https://pypi.org/project/build/"))
         self.assertFalse(url_status("youtube.com"))
         self.assertFalse(url_status("youtube"))
         self.assertFalse(url_status("https://www.youtubeeeee.com"))
         self.assertFalse(url_status("#"))
 
+    def test_internal_links(self):
+        url = "http://olympus.realpython.org/profiles"
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        gather_links(url, soup)
+        self.assertEqual(len(internal_links),3)
 
